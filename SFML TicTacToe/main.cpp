86781da2cddba7ae695ff57418b941ea185842a3
pananananas <drumpixtv@gmail.com>
2022-06-10 11:03:13 +0200

    Initial Commit

---
+++ SFML TicTacToe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SFML TicTacToe
+//
+//  Created by Eryk Wójcik on 10/06/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
