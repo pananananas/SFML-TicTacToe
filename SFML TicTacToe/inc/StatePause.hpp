7e8af6cf76183eaa03683149a7bdf560cf4f4711
pananananas <drumpixtv@gmail.com>
2022-06-18 22:52:54 +0200

    added retry button on pause state

--- SFML TicTacToe/inc/StatePause.hpp
+++ SFML TicTacToe/inc/StatePause.hpp
@@ -9,6 +9,7 @@ class StatePause: public State {
     GameDataRef _data;
     sf::Sprite  _background;
     sf::Sprite  _resumeButton;
+    sf::Sprite  _retryButton;
     sf::Sprite  _homeButton;
     sf::Sprite  _title;
     
--- SFML TicTacToe/src/StatePause.cpp
+++ SFML TicTacToe/src/StatePause.cpp
@@ -15,7 +15,9 @@ void StatePause:: Init() {
     this -> _data -> assets.LoadTexture("Resume Button",    RESUME_BUTTON);
     this -> _data -> assets.LoadTexture("Home Button",      HOME_BUTTON);
     this -> _data -> assets.LoadTexture("P Title",          PAUSE_TITLE_FILEPATH);
+    this -> _data -> assets.LoadTexture("Replay Button", RETRY_BUTTON );
 
+    this -> _retryButton.setTexture( this -> _data -> assets.GetTexture("Replay Button") );
     this -> _background.setTexture(   this -> _data -> assets.GetTexture("Pause Background"));
     this -> _resumeButton.setTexture( this -> _data -> assets.GetTexture("Resume Button"));
     this -> _homeButton.setTexture(   this -> _data -> assets.GetTexture("Home Button"));
@@ -28,8 +30,14 @@ void StatePause:: Init() {
 
     this -> _homeButton.setPosition( this -> _data -> window.getSize().x / 2
                                    - this -> _homeButton.getLocalBounds().width / 2 ,
-                                     this -> _data -> window.getSize().y / 5 * 3
+                                     this -> _data -> window.getSize().y / 5 * 4
                                    - this -> _homeButton.getLocalBounds().height / 2);
+    
+    this -> _retryButton.setPosition( this -> _data -> window.getSize().x / 2
+                                   - this -> _retryButton.getLocalBounds().width / 2 ,
+                                     this -> _data -> window.getSize().y / 5 * 3
+                                   - this -> _retryButton.getLocalBounds().height / 2);
+    
     this -> _title.setPosition((SCREEN_WIDTH/ 2) - (this -> _title.getGlobalBounds().width / 2), 0);
 }
 
@@ -50,6 +58,8 @@ void StatePause:: HandleInput() {
             this -> _data -> machine.RemoveState();
             this -> _data -> machine.AddState(StateRef(new StateMainMenu(_data, _size,_winSize, _isPlayerX, _VSAI)), true);
         }
+        if ( this -> _data -> input.IsSpriteClicked(this -> _retryButton, sf::Mouse::Left, this -> _data -> window) )
+            this -> _data -> machine.AddState(StateRef( new StateGame(_data, _size,_winSize, _isPlayerX, _VSAI) ), true);  // End Game
     }
 }
 
@@ -64,5 +74,6 @@ void StatePause:: Draw(float dt) {
     this -> _data -> window.draw( this -> _resumeButton);
     this -> _data -> window.draw( this -> _homeButton);
     this -> _data -> window.draw( this -> _title);
+    this -> _data -> window.draw( this -> _retryButton);
     this -> _data -> window.display();
 }
